aa0f45999f7d38c8524dda25be3d2ac0edf2e569
ElectroMagneticSpectrum <tiontension21@gmail.com>
2020-08-02 20:21:11 +0530

    Added some epicccc stuff
    added an epic store

---
+++ Cassete Mechanic/Assets/Scripts/Spawn_Cassette.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Cassette : MonoBehaviour
+{
+    public GameObject Cassette;
+
+    public GameObject Spawn_Point;
+
+    public Vector3 Spawn_Point_Location;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Spawn_Point_Location = Spawn_Point.transform.position; 
+    }
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(Cassette,Spawn_Point_Location, Quaternion.identity);
+
+
+        }
+        
+    }
+
+  
+}
