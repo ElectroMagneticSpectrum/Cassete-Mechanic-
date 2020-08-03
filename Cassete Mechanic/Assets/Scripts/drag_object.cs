8756a811d27340e850db5cfe5200e407ad683070
ElectroMagneticSpectrum <tiontension21@gmail.com>
2020-08-03 11:09:33 +0530

    added constrains

--- Cassete Mechanic/Assets/Scripts/drag_object.cs
+++ Cassete Mechanic/Assets/Scripts/drag_object.cs
@@ -8,6 +8,8 @@ public class drag_object : MonoBehaviour
     public  float Start_PositionY;
     public  bool held = false;
 
+    public Vector3 WhenDragOut;
+
 
     void Start()
     {
@@ -25,6 +27,34 @@ public class drag_object : MonoBehaviour
 
 
         this.gameObject.transform.localPosition = new Vector3(Mouse_Position.x - Start_PositionX,Mouse_Position.y - Start_PositionY,0 );
+
+        WhenDragOut.x = 2f;
+        WhenDragOut.y = -4f;
+        WhenDragOut.z = 0f;
+        
+
+
+
+
+
+        if(transform.position.x >= 10  ){
+              transform.position = WhenDragOut ;
+
+        }
+
+        if(transform.position.x <= -5.74f  ){
+              transform.position = WhenDragOut ;
+
+        }
+
+        if(transform.position.y >= -2.52f  ){
+              transform.position = WhenDragOut ;
+
+        }
+        if(transform.position.y  <= -5.54f  ){
+              transform.position = WhenDragOut ;
+
+        }
 
 
         }
